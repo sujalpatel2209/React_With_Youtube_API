7f4b99a09dbc5bc231f8b2d0706f24a1fbdc1945
sujalpatel2209 <sujalpatel022@gmail.com>
2017-09-16 13:34:22 +0530

    setup project and install youtube api search package

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyAz_MQN4oy6PGMgM8IAOrm-lKU1SNTHtqw';
+
+const App = function () {
+    return <div>Hello World..!!!</div>;
+}
+
+ReactDOM.render(<App />, document.getElementById('container'));
