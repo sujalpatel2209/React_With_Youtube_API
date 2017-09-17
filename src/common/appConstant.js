b5fbee0d25a91496445dca7f8bb9b228280c96f8
sujalpatel2209 <sujalpatel022@gmail.com>
2017-09-17 18:14:35 +0530

    ~ create constant file for fetch youtub api key

---
+++ src/common/appConstant.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const YOUTUBE_API_KEY = 'AIzaSyAz_MQN4oy6PGMgM8IAOrm-lKU1SNTHtqw';
--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
-const API_KEY = 'AIzaSyAz_MQN4oy6PGMgM8IAOrm-lKU1SNTHtqw';
+import { YOUTUBE_API_KEY } from './common/appConstant';
+
+
+YTSearch({
+    key: YOUTUBE_API_KEY,
+    term: 'surfboards'
+}, function (data) {
+    console.log(data)
+})
 
 const App = function () {
     return (
