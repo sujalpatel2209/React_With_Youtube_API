9c3dd5bafe65057aeced5f19b47ed92967a65a70
sujalpatel2209 <sujalpatel022@gmail.com>
2017-09-21 22:13:19 +0530

    ~ create list item component ~ pass single video data in list item component using map()

---
+++ src/components/video_list_item.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class ListItem extends Component {
+
+    render() {
+        return (
+            <li>Video</li>
+        );
+    }
+}
+
+export default ListItem;
