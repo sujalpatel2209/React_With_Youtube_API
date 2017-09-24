49144ad51770649c5d3f89fce28f2713dea56df4
sujalpatel2209 <sujalpatel022@gmail.com>
2017-09-24 13:24:03 +0530

    ~ Design detail page ~ add detail component in index js ~ pass video single object in video detail page

---
+++ src/components/video_detail.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const videoDetail = ({video}) => {
+
+    if(!video){
+        return <div>Loading...</div>
+    }
+
+    const videoId = video.id.videoId;
+    const videoUrl = `https://www.youtube.com/embed/${videoId}`;
+
+    return (
+        <div className="video-detail col-md-8">
+            <div className="embed-responsive embed-responsive-16by9">
+                <iframe src={ videoUrl } frameborder="0" className="embed-responsive-item"></iframe>
+            </div>
+            <div className="details">
+                <div>{ video.snippet.title }</div>
+                <div>{ video.snippet.description }</div>
+            </div>
+        </div>
+    );
+}
+
+export default videoDetail;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import {YOUTUBE_API_KEY} from './common/appConstant';
+import VideoDetail from './components/video_detail';
 
 
 class App extends Component {
@@ -26,6 +27,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoDetail video={this.state.videos[0]}/>
                 <VideoList videos={this.state.videos}/>
             </div>
         );
