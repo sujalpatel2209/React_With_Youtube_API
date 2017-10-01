71f45249443a62b509b10ccb2f62940310d85553
sujalpatel2209 <sujalpatel022@gmail.com>
2017-10-01 17:34:33 +0530

    ~ write css

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -11,7 +11,7 @@ class SearchBar extends Component {
 
     render() {
         return (
-            <div>
+            <div className='search-bar'>
                 <input value={this.state.term} onChange={this.onInputChange}/>
             </div>
         );
